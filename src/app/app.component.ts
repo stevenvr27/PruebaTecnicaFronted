4c6c1a0b365895de200c5fcde6010fa2975a2cee
steven vargas <114686723+stevenvr27@users.noreply.github.com>
2024-02-08 18:39:38 -0600

    Comitttoday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PokemonPictureComponent } from "./pokemon-picture/pokemon-picture.component";
-import { TargetComponent } from "./target/target.component";
+import { PokemonPictureComponent } from './pokemon-picture/pokemon-picture.component';
+import { TargetComponent } from './target/target.component';
+import { PokemonService } from '../app/service/pokemon.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, PokemonPictureComponent, TargetComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'], // Note: Corrected styleUrl to styleUrls
+  imports: [
+    RouterOutlet,
+    PokemonPictureComponent,
+    TargetComponent,
+    HttpClientModule,
+    CommonModule,
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Ensure you implement OnInit
   title = 'pokedex';
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    this.pokemonService.getpokemons().subscribe({
+      next: (pokemons: any) => {
+        console.log('Pokemons:', pokemons);
+      },
+      error: (error) => {
+        console.error('Error fetching pokemons:', error);
+      },
+    });
+  }
 }
--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -1,16 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
- 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private hhtpClient:HttpClient) { }
-
-
-getpokemons(){
-  return this.hhtpClient.get('localhost:3000/api/pokemon');
-}
+  getpokemons() {
+    return this.httpClient.get('http://localhost:3000/api/pokemon');
+  }
 }
